368725cc8cfba156af69065d40240bebe4c91ab7
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2024-09-05 06:19:39 -0700

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import MainBanner from "../components/MainBanner";
-import Navbar from "../components/Navbar";
-import Place from "../components/Place";
+
+
 
 export default function Home() {
   return (
